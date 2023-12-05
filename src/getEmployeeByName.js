e5d9d5e00117e7f6b5837241f75f5d5f5c98c14f
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-12-05 12:21:12 +0000

    getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,14 +2,15 @@ const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
   // seu código aqui
-  const findEmployee = data.employees.find((person) => employeeName === person.firstName || employeeName === person.lastName);
+  const findEmployee = data.employees
+    .find((person) => employeeName === person.firstName || employeeName === person.lastName);
   return findEmployee || {};
 };
 
 // Retorne um objeto vazio caso a função não receba parâmetros;
 // Retorne as informações da pessoa colaboradora caso o parâmetro seja igual ao nome ou igual ao último nome;
-// data.employees pra acessar 
+// data.employees pra acessar.
 // percorrer esse array e encontrar a pessoa pelo employeeName === fistName || === lastNme;
-// retornar o objeto com as informações ou vazio 
+// retornar o objeto com as informações ou vazio.
 
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,11 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...ids) => {  //os tres pontinhos deixam a função receber parametros variáveis e retornam como array
+const getSpeciesByIds = (...ids) => { // os tres pontinhos deixam a função receber parametros variáveis e retornam como array
   const animalsId = data.species.filter((elemento) => ids.includes(elemento.id));
   // filter para filtrar os elementos pelo parametro escolhido
-  // includes serve pra ver se o elemento atual está no array pra incluir ele no final do filter 
+  // includes serve pra ver se o elemento atual está no array pra incluir ele no final do filter
   // includes filtra o data.species e verifica se o id está lá e ai inclui ele no array novo do filter
-  // não sei como retirar essa constante e esse return, não funciona. 
+  // não sei como retirar essa constante e esse return, não funciona.
   return animalsId;
 };
 
