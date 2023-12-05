a1ecc9e5286fab8e1714ec3b67604e0e040b5bbb
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-12-05 11:36:18 +0000

    getSpeciesByIds

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
   // seu código aqui
+  data.species((animal))
 };
+
+
 
 module.exports = getAnimalsOlderThan;
