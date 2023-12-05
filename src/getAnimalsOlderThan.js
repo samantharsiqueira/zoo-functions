e1cdca68fd62c0bf8afd6534dbf4368126f0a958
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-12-05 12:16:55 +0000

    getEmployeeByName

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,12 +3,12 @@ const data = require('../data/zoo_data');
 const getAnimalsOlderThan = (animal, age) => {
   // Nessa linha eu tô procurando o animal e vendo se o nome dele bate com a espécie.
   const findAnimal = data.species.find((specie) => specie.name === animal);
-  // Nessa eu to vendo se o animal que eu encontrei acima tem na parte de residentes (onde tão as informações) a idade correta 
+  // Nessa eu to vendo se o animal que eu encontrei acima tem na parte de residentes (onde tão as informações) a idade correta.
   return findAnimal.residents.every((specie) => specie.age >= age);
 };
 
 // receber uma espécie e uma idade e retornar se todos os animais tem a idade ou são mais velhos
-// retornar um valor boleano?
+// retornar um valor boleano????
 // Eu preciso encontrar o animal find ou filter
 // verificar se eles tem a idade ou mais.
 // retornar true or false com every pq são todos os animais.
