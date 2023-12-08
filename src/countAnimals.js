974625a3da0865c9669fa0100d68fcface74ae5d
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-12-08 17:50:49 +0000

    getOldestFromFristSpecies

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,8 +1,36 @@
 const data = require('../data/zoo_data');
 
+const zoo = data.species;
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  // Se parametro for vazio, retorna um objeto com todos os animais.
+  if (!animal) {
+    return Object.keys(zoo).reduce((animal, species) => {
+      result[species] = zoo.length;
+      return animal;
+    }, {});
+  }
 
+  // Se o parâmetro `species` for fornecido, retornar a contagem para essa espécie
+  if (animal.species && zoo[animal.species]) {
+    // Se a chave `sex` também for fornecida, filtrar por sexo
+    if (animal.sex) {
+      return zoo[animal.species].reduce((count, animal) => {
+        if (animal.sex === params.sex) {
+          return count + 1;
+        }
+        return count;
+      }, 0);
+    }
+    // Se apenas a espécie for fornecida, retornar a contagem total
+    return zoo[animal.species].length;
+  }
 };
-
 module.exports = countAnimals;
+
+// Criar uma função que retorne a quantidade de animais { species: 'penguins' };
+// Retorno será um numero
+// A quantidade eh o tamanho do array
+// Pode filtrar por sexo retornar um numero com a qntde da especie e sexo
+// Sem parametro deve retornar todas as espécies e a quantidade de residentes de cada uma;
+// reduce volta um objeto;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  // Primeiro eu preciso achar o employee pelo ID;
+  const findEmployee = data.employees
+    .find((employee) => employee.id === id);
+    // Encontrar a primeira espécie que ele é responsavél no array;
+  const firstspecie = findEmployee.responsibleFor[0];
+  // Pegar as informações da espécie que eu achei pra poder trabalhar com elas;
+  const findSpecieId = data.species.find((specie) => specie.id === firstspecie);
+  // Se o ID Do animal for igual ao encontrado acima, vamos por o reduce nele pra achar o mais velho;
+  if (findSpecieId) {
+    const oldestAnimal = findSpecieId.residents.reduce((acumulador, valorAtual) => {
+      // verifica se a maior idade está no acumulador ou no valor atual pra retornar;
+      if (!acumulador || valorAtual.age > acumulador.age) {
+        return valorAtual;
+      }
+      return acumulador;
+    });
+    // Retorna o array do oldestAnimal
+    return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
+  }
 };
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+
 module.exports = getOldestFromFirstSpecies;
+
+// Encontrar o employee pelo ID.
+// Encontra a primeira espécie de animal que o employee é responsavél
+// find o animail mais velho
+// retornar um array com informações do oldestAnimal com nome, sexo e idade;
